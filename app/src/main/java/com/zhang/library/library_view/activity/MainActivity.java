9e6d946c28766644662df3d5bb6c81fc619445e7
ZhangXiaoMing <zhangxiaoming6672@163.com>
2021-07-11 16:18:20 +0800

    icon图标替换，示例修改

--- app/src/main/java/com/zhang/library/library_view/activity/MainActivity.java
+++ app/src/main/java/com/zhang/library/library_view/activity/MainActivity.java
@@ -4,27 +4,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.zhang.library.library_view.BuildConfig;
 import com.zhang.library.library_view.R;
-import com.zhang.library.library_view.adapter.TestAdapter;
-import com.zhang.library.library_view.model.TestModel;
 import com.zhang.library.utils.LogUtils;
 import com.zhang.library.utils.context.ContextUtils;
-import com.zhang.library.view.XMSlideMarqueeView;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
-
-    private TestAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,44 +35,8 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "六边形图片", Toast.LENGTH_SHORT).show();
             }
         });
-
-
-        XMSlideMarqueeView rv_content = findViewById(R.id.rv_content);
-
-        rv_content.addOnScrollListener(new RecyclerView.OnScrollListener() {
-            @Override
-            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
-                super.onScrollStateChanged(recyclerView, newState);
-                LogUtils.debug(TAG, "RecyclerView.OnScrollListener>>>onScrollStateChanged()>>>newState = %d", newState);
-            }
-
-            @Override
-            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
-                super.onScrolled(recyclerView, dx, dy);
-                LogUtils.debug(TAG,
-                        "RecyclerView.OnScrollListener>>>onScrolled()>>>scrollState = %d,dx = %d, dy = %d",
-                        recyclerView.getScrollState(), dx, dy);
-            }
-        });
-
-        rv_content.setAdapter(getAdapter());
     }
 
     private void initData() {
-        int size = new Random().nextInt(20) + 20;
-
-        List<TestModel> list = new ArrayList<>();
-        for (int i = 0; i < size; i++) {
-            list.add(new TestModel());
-        }
-        getAdapter().getDataHolder().setDataList(list);
-        ((XMSlideMarqueeView) findViewById(R.id.rv_content)).start();
-    }
-
-    private TestAdapter getAdapter() {
-        if (mAdapter == null) {
-            mAdapter = new TestAdapter();
-        }
-        return mAdapter;
     }
 }
