ba5d54949b390f441925bd3b234456775a0e800e
xiaoming6672 <zhangxiaoming6672@163.com>
2023-08-25 21:37:54 +0800

    新增XMAutoFitStatusHeightView

---
+++ lib/src/main/java/com/zhang/library/view/XMAutoFitStatusHeightView.java
@@ -0,0 +1,39 @@
+package com.zhang.library.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Space;
+
+import com.zhang.library.utils.context.ResUtils;
+
+/**
+ * 自动适应状态栏高度的View。仿照{@link Space}不显示任何内容
+ *
+ * @author ZhangXiaoMing 2023-02-13 14:19 周一
+ */
+public class XMAutoFitStatusHeightView extends View {
+
+    public XMAutoFitStatusHeightView(Context context) {
+        super(context);
+    }
+
+    public XMAutoFitStatusHeightView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public XMAutoFitStatusHeightView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int height = ResUtils.getStatusBarHeight();
+        setMeasuredDimension(1, height);
+    }
+}
